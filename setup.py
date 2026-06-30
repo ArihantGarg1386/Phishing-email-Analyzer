64dc1974c5d48552b0e11df6f29484d83abfb991
ArihantGarg1386 <arihant1386@gmail.com>
2026-06-30 19:06:35 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Phishing-email-Analyzer",
-    version="1.0.0",
+    version="1.0",
     description="Static analysis + threat-intel verification tool for detecting phishing in raw emails",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
